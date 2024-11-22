5c75639707d4f31462b5bd3990c9370bcac95b9e
francoleon08 <francoleon284@gmail.com>
2024-11-22 19:54:52 -0300

    fixed: cors movies service

--- movies/src/server.js
+++ movies/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const movieRoutes = require('./routes/routes');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
